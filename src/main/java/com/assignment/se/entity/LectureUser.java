79cccb94e38613435213fac7ed725ef04898588a
grayroom <leejh9226@gmail.com>
2023-06-16 03:49:56 +0900

    :sparkles: 성적관련 API추가, API권한분리

--- src/main/java/com/assignment/se/entity/LectureUser.java
+++ src/main/java/com/assignment/se/entity/LectureUser.java
@@ -15,12 +15,12 @@ public class LectureUser {
 	private Long lecture_user_id;
 
 	@ManyToOne
-	@JoinColumn(name = "course_id")
+	@JoinColumn(name = "course")
 	private Course course;
 
 	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private UserAuth userAuth;
+	@JoinColumn(name = "user")
+	private UserAuth user;
 
 	private Long grade;
 	private Long semester;
